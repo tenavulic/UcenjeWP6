1bac0e4058bd5ee1a466c241d8d873f8fd8ec4c2
Tena Vulić <tena.vulic@gmail.com>
2024-11-11 18:32:54 +0100

    Kreiranje tablice grupe

--- skriptawp6.sql
+++ skriptawp6.sql
@@ -5,10 +5,17 @@
 
 --use edunovawp6;
 
-create table smjerovi(
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--izvodiseod datetime,
+--vaucer bit
+--);
+create table grupe(
 sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-izvodiseod datetime,
-vaucer bit
+naziv varchar (20),
+velicinagrupe int,
+predavac varchar (50),
+smjer int
 );
